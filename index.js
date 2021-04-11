47fa6ba70e6471da52d98bbf0cc9ac413023b6a0
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-04-11 20:38:58 +0200

    starting point, with configs n stuff

---
+++ index.js
@@ -0,0 +1,53 @@
+const fs = require('file-system');
+const package = require('./package.json');
+const { Parser } = require('./parser/index.js');
+
+var config = {
+    "compilerVersion": package.version,
+    "inDir": "./",
+    "outDir": "./"
+};
+
+var parser;
+
+async function main() {
+    await setup().catch(e => {
+        throw new Error(e);
+    });
+    console.log(config)
+    if (fs.existsSync(config.inDir)) {
+        fs.mkdirSync(config.outDir);
+        parser = new Parser(config);
+        parser.initDir();
+    }
+    else {
+        throw new Error("InputDir Does Not Exist");
+    }
+}
+
+function setup() {
+    return new Promise(async (resolve, reject) => {
+        var error = [];
+        var args = process.argv.slice(2);
+        if (args.length > 0) {
+            if (fs.existsSync('./scar.config.json')) {
+                console.log("Ignoring Duplicate Config Entries and Preffering args");
+            }
+            console.log(args)
+        }
+        else {
+            if (fs.existsSync('./scar.config.json')) {
+                config = JSON.parse(fs.readFileSync('./scar.config.json'));
+            }
+            else {
+                console.log("using Standard Config");
+            }
+        }
+        if (error.length > 0) {
+            reject("Something Went Wrong with the Setup");
+        }
+        else resolve();
+    })
+}
+
+main();
