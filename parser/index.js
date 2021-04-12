96a863bb93ada5327c4b49deacdfb867843787ad
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-04-12 19:42:53 +0200

    added the Syntax Checker and reading files now properly

--- parser/index.js
+++ parser/index.js
@@ -1,9 +1,12 @@
 const fs = require('file-system');
 
+const { Syntax } = require('../syntax/')
+
 class Parser {
 
     constructor(conf) {
         this.config = conf;
+        this.Syntax = new Syntax(conf);
     }
 
     async listFiles() {
@@ -22,14 +25,12 @@ class Parser {
                 console.log(files)
                 if(files.filter(file => !file.includes('.scar')).length > 0) {
                     var wrongEnding = [];
-                    files.filter(file => {
-                        if(!file.includes('.scar')) {
-                            wrongEnding.push(file);
-                        }
+                    files = files.filter(file => {
+                        if(!file.includes('.scar')) wrongEnding.push(file);
                         return file.includes('.scar');
                     });
-                    console.log(wrongEnding, files)
                     if(wrongEnding.length > 0) console.log(`Some Files had the Wrong Ending`, `\nAffected Files: \n`, ...wrongEnding, `\n Simply just copying these files`);
+                    this.getFiles(files)
                 } 
                 if(!files.length) {
                     reject(`There Are no Files in the Input Dir`);
@@ -45,7 +46,8 @@ class Parser {
     async getFiles(files) {
         return new Promise(async (resolve, reject) => {
             files.forEach(async (file) => {
-                var loadedFile = fs.readFileSync(file);
+                var loadedFile = fs.readFileSync(`${this.config.inDir}/${file}`, "utf8");
+                await this.Syntax.checkSyntax(loadedFile);
             })
         })
     }
